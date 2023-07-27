ce465520bc0890094da0c094618105a92c355e25
itszshivam <139079474+itszshivam@users.noreply.github.com>
2023-07-28 01:02:29 +0530

    Update __main__.py

--- Zaid/__main__.py
+++ Zaid/__main__.py
@@ -7,10 +7,10 @@ from Zaid import clients, app, ids
 
 async def start_bot():
     await app.start()
-    print("LOG: Founded Bot token Booting™°‌ 🫧 🇴 🇽 𝐘 𝐆 𝐄 𝐍..")
+    print("LOG: Founded Bot token Booting ◉ 𝐎𝐕𝐄𝐑 𝐏𝐎𝐖𝐄𝐑𝐄𝐃 ◉")
     for all_module in ALL_MODULES:
         importlib.import_module("Zaid.modules" + all_module)
-        print(f"Successfully Imported ™°‌ 🫧 🇴 🇽 𝐘 𝐆 𝐄 𝐍 {all_module} 💥")
+        print(f"Successfully Imported ◉ 𝐎𝐕𝐄𝐑 𝐏𝐎𝐖𝐄𝐑𝐄𝐃 ◉ {all_module} 💥")
     for cli in clients:
         try:
             await cli.start()
